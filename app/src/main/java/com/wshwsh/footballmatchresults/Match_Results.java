b44d5bcae65bd42f50daa6ee8ad670650e3d930e
mahmoudkida <mahmoudkida@gmail.com>
2018-04-22 14:26:29 +0300

    Initial commit

---
+++ app/src/main/java/com/wshwsh/footballmatchresults/Match_Results.java
@@ -0,0 +1,15 @@
+package com.wshwsh.footballmatchresults;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Match_Results extends AppCompatActivity {
+
+    String teamANmae= "Liverpool";
+    String teamBName="Manchester United";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match__results);
+    }
+}
