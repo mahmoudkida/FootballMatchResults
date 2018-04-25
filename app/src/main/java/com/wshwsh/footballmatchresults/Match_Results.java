7407776a87fa1c53029eda9c0a1612e497be74c1
mahmoudkida <mahmoudkida@gmail.com>
2018-04-25 17:13:10 +0300

    fix:     -Fix app in horizontal mode     -Fix extra spaces     -Fix margin and padding from sp to dp     -Fix vague variable name

--- app/src/main/java/com/wshwsh/footballmatchresults/Match_Results.java
+++ app/src/main/java/com/wshwsh/footballmatchresults/Match_Results.java
@@ -8,57 +8,55 @@ import android.widget.TextView;
 import java.lang.reflect.Array;
 
 public class Match_Results extends AppCompatActivity {
+    String teamANmae = "Liverpool";
+    String teamBName = "Manchester United";
 
-    String teamANmae= "Liverpool";
-    String teamBName="Manchester United";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match__results);
     }
 
-
-
     public void addGoalTeamA(View view) {
         TextView teamAGoals = (TextView) findViewById(R.id.teamAGoals);
-        teamAGoals.setText(String.valueOf(Integer.parseInt(teamAGoals.getText().toString())+1));
-
+        teamAGoals.setText(String.valueOf(Integer.parseInt(teamAGoals.getText().toString()) + 1));
     }
+
     public void addFoulTeamA(View view) {
         TextView teamAFouls = (TextView) findViewById(R.id.teamAFouls);
-        teamAFouls.setText(String.valueOf(Integer.parseInt(teamAFouls.getText().toString())+1));
+        teamAFouls.setText(String.valueOf(Integer.parseInt(teamAFouls.getText().toString()) + 1));
     }
+
     public void increasePossessionTeamA(View view) {
         TextView teamAPossession = (TextView) findViewById(R.id.teamAPossession);
         TextView teamBPossession = (TextView) findViewById(R.id.teamBPossession);
-        teamAPossession.setText(String.valueOf(Integer.parseInt(teamAPossession.getText().toString().split("%")[0])+1)+"%");
-        teamBPossession.setText(String.valueOf(Integer.parseInt(teamBPossession.getText().toString().split("%")[0])-1)+"%");
+        teamAPossession.setText(String.valueOf(Integer.parseInt(teamAPossession.getText().toString().split("%")[0]) + 1) + "%");
+        teamBPossession.setText(String.valueOf(Integer.parseInt(teamBPossession.getText().toString().split("%")[0]) - 1) + "%");
     }
 
     public void addGoalTeamB(View view) {
         TextView teamBGoals = (TextView) findViewById(R.id.teamBGoals);
-        teamBGoals.setText(String.valueOf(Integer.parseInt(teamBGoals.getText().toString())+1));
+        teamBGoals.setText(String.valueOf(Integer.parseInt(teamBGoals.getText().toString()) + 1));
     }
 
     public void addFoulTeamB(View view) {
         TextView teamBFouls = (TextView) findViewById(R.id.teamBFouls);
-        teamBFouls.setText(String.valueOf(Integer.parseInt(teamBFouls.getText().toString())+1));
+        teamBFouls.setText(String.valueOf(Integer.parseInt(teamBFouls.getText().toString()) + 1));
     }
 
     public void increasePossessionTeamB(View view) {
         TextView teamAPossession = (TextView) findViewById(R.id.teamAPossession);
         TextView teamBPossession = (TextView) findViewById(R.id.teamBPossession);
-        teamAPossession.setText(String.valueOf(Integer.parseInt(teamAPossession.getText().toString().split("%")[0])-1)+"%");
-        teamBPossession.setText(String.valueOf(Integer.parseInt(teamBPossession.getText().toString().split("%")[0])+1)+"%");
+        teamAPossession.setText(String.valueOf(Integer.parseInt(teamAPossession.getText().toString().split("%")[0]) - 1) + "%");
+        teamBPossession.setText(String.valueOf(Integer.parseInt(teamBPossession.getText().toString().split("%")[0]) + 1) + "%");
     }
 
     public void resetStates(View view) {
-        String[] Ids = {"teamAGoals","teamAFouls","teamBGoals","teamBFouls"};
-        for(int i=0;i<Ids.length;i++) {
-            TextView textView = (TextView) findViewById(getResources().getIdentifier(Ids[i], "id", getPackageName()));
+        String[] ViewsIdsToBeResetReseted = {"teamAGoals", "teamAFouls", "teamBGoals", "teamBFouls"};
+        for (int i = 0; i < ViewsIdsToBeResetReseted.length; i++) {
+            TextView textView = (TextView) findViewById(getResources().getIdentifier(ViewsIdsToBeResetReseted[i], "id", getPackageName()));
             textView.setText("0");
         }
-
         TextView textView = (TextView) findViewById(R.id.teamAPossession);
         textView.setText("50%");
         textView = (TextView) findViewById(R.id.teamBPossession);
